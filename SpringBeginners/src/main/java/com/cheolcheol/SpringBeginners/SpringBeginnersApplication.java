40743937dd3c834dd7eb4bc063bfcfca53aab2ec
Cheol-Cheol <dream174822@gmail.com>
2023-01-16 13:52:07 +0900

    📍스프링 입문, 복습 시작

--- SpringBeginners/src/main/java/com/cheolcheol/SpringBeginners/SpringBeginnersApplication.java
+++ SpringBeginners/src/main/java/com/cheolcheol/SpringBeginners/SpringBeginnersApplication.java
@@ -1,13 +1,13 @@
-package hello.hellospring;
+package com.cheolcheol.SpringBeginners;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloSpringApplication {
+public class SpringBeginnersApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloSpringApplication.class, args);
+		SpringApplication.run(SpringBeginnersApplication.class, args);
 	}
 
 }
--- SpringBeginners/src/test/java/com/cheolcheol/SpringBeginners/SpringBeginnersApplicationTests.java
+++ SpringBeginners/src/test/java/com/cheolcheol/SpringBeginners/SpringBeginnersApplicationTests.java
@@ -1,10 +1,10 @@
-package hello.hellospring;
+package com.cheolcheol.SpringBeginners;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HelloSpringApplicationTests {
+class SpringBeginnersApplicationTests {
 
 	@Test
 	void contextLoads() {
