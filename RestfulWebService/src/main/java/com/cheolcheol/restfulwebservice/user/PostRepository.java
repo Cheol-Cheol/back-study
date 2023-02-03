47b3452009cf20ca4e89db8618e966a5f1b84c65
Cheol-Cheol <dream174822@gmail.com>
2023-02-03 12:39:16 +0900

    📍Spring Boot를 이용한 RESTful Web Services 개발 - 복습 시작

--- RestfulWebService/src/main/java/com/cheolcheol/restfulwebservice/user/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cheolcheol.restfulwebservice.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PostRepository extends JpaRepository<PostDomain, Integer> {
-}
