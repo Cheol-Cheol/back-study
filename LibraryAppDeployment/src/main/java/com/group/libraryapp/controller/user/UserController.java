35fdb6a944d1171f0e9cf3555e5eb1c06e48ca1e
Cheol-Cheol <dream174822@gmail.com>
2023-02-11 11:40:29 +0900

    📍생애 최초 JPA 사용하기

--- LibraryAppDeployment/src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ LibraryAppDeployment/src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -3,23 +3,18 @@ package com.group.libraryapp.controller.user;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
-import com.group.libraryapp.repository.user.UserRepository;
-import com.group.libraryapp.service.fruit.FruitService;
-import com.group.libraryapp.service.user.UserService;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.jdbc.core.JdbcTemplate;
+import com.group.libraryapp.service.user.UserServiceV1;
+import com.group.libraryapp.service.user.UserServiceV2;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class UserController {
-    private final UserService userService;
-    private final FruitService fruitService;
+    private final UserServiceV2 userService;
 
-    public UserController(UserService userService, @Qualifier("main") FruitService fruitService) {
+    public UserController(UserServiceV2 userService) {
         this.userService = userService;
-        this.fruitService = fruitService;
     }
 
     @PostMapping("/user")
