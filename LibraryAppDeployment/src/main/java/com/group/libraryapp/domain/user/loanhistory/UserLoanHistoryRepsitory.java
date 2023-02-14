cfd2a230e23dd118a67cdbfd8b81213e8dd55e6c
Cheol-Cheol <dream174822@gmail.com>
2023-02-14 21:54:03 +0900

    🎉프로젝트 생성

--- LibraryAppDeployment/src/main/java/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepsitory.java
+++
@@ -1,9 +0,0 @@
-package com.group.libraryapp.domain.user.loanhistory;
-
-import com.group.libraryapp.domain.user.UserRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserLoanHistoryRepsitory extends JpaRepository<UserRepository, Long> {
-    // select * from user_loan_history where book_name = ? and is_return = ?
-    boolean existsByBookNameAndIsReturn(String name, boolean isReturn);
-}
