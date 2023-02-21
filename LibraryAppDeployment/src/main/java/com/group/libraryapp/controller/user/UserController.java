d77ee91886456b4c4eff6815ee5bd51d59af58cb
Cheol-Cheol <dream174822@gmail.com>
2023-02-21 16:39:06 +0900

    📍생애 최초 Database 조작하기

--- LibraryAppDeployment/src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ LibraryAppDeployment/src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -1,33 +1,62 @@
 package com.group.libraryapp.controller.user;
 
-import com.group.libraryapp.domain.user.Fruit;
-import com.group.libraryapp.domain.user.User;
 import com.group.libraryapp.dto.user.reponse.UserResponse;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.group.libraryapp.dto.user.request.UserUpdateRequest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class UserController {
-    private final List<User> users = new ArrayList<>();
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @PostMapping("/user")
     public void saveUser(@RequestBody UserCreateRequest request) {
-        users.add(new User(request.getName(), request.getAge()));
+        String sql = "INSERT INTO user (name, age) VALUES (?, ?)";
+        jdbcTemplate.update(sql, request.getName(), request.getAge());
     }
 
     @GetMapping("/user")
     public List<UserResponse> getUser() {
-        List<UserResponse> responses = new ArrayList<>();
-        // users -> userReponses로 변환
-        for (int i = 0; i < users.size(); i++) {
-            responses.add(new UserResponse(i + 1, users.get(i)));
+        String sql = "SELECT * FROM user";
+        return jdbcTemplate.query(sql, (rs, rowNum) -> {
+            long id = rs.getLong("id");
+            String name = rs.getString("name");
+            int age = rs.getInt("age");
+
+            return new UserResponse(id, name, age);
+        });
+    }
+
+    @PutMapping("/user")
+    public void updateUser(@RequestBody UserUpdateRequest request) {
+        String readSql = "SELECT * FROM user WHERE id = ?";
+        // 비어있다면 0을 출력(거짓), 값이 존재한다면 0이 담긴 리스트가 반환되면서 1을 출력(참)
+        boolean isUserNotExist = jdbcTemplate.query(readSql, (rs, rowNum) -> 0, request.getId()).isEmpty();
+        if (isUserNotExist) {
+            throw new IllegalArgumentException();
         }
-        return responses;
+
+        String sql = "UPDATE user SET name = ? WHERE id = ?";
+        jdbcTemplate.update(sql, request.getName(), request.getId());
+    }
+
+    @DeleteMapping("/user")
+    public void deleteUser(@RequestParam String name) {
+        String readSql = "SELECT * FROM user WHERE name = ?";
+        // 비어있다면 0을 출력(거짓), 값이 존재한다면 0이 담긴 리스트가 반환되면서 1을 출력(참)
+        boolean isUserNotExist = jdbcTemplate.query(readSql, (rs, rowNum) -> 0, name).isEmpty();
+        if (isUserNotExist) {
+            throw new IllegalArgumentException();
+        }
+
+        String sql = "DELETE FROM user WHERE name = ?";
+        jdbcTemplate.update(sql, name);
     }
 }
